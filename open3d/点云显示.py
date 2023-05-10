a7ee54b5cdb3b52abf3d885e4656beb03ddd41cd
MR-520DAI <dai857023@163.com>
2023-05-10 17:00:38 +0800

    点云块融合

--- open3d/点云显示.py
+++ open3d/点云显示.py
@@ -1,6 +1,15 @@
 import open3d as o3d
 import numpy as np
 
-pcd = o3d.io.read_point_cloud("integrated.ply")
+pcd = o3d.io.read_point_cloud("data\\integrated.ply")
+pcd1 = o3d.io.read_point_cloud("data\\integrated.ply")
+
+pcd += pcd1
+# p1 = np.asarray(pcd.points)
+# print(p1[0])
+# pcd.transform([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0.1, 1]])
+# p2 = np.asarray(pcd.points)
+# print(p2[0])
+
 pcd.voxel_down_sample(voxel_size=0.1)
 o3d.visualization.draw_geometries([pcd])
