9e36510eae4ba97e8e7c5501de1bf3d38d1510be
MR-520DAI <dai857023@163.com>
2023-05-11 09:46:42 +0800

    open3d学习

--- open3d/点云显示.py
+++ open3d/点云显示.py
@@ -4,12 +4,5 @@ import numpy as np
 pcd = o3d.io.read_point_cloud("data\\integrated.ply")
 pcd1 = o3d.io.read_point_cloud("data\\integrated.ply")
 
-pcd += pcd1
-# p1 = np.asarray(pcd.points)
-# print(p1[0])
-# pcd.transform([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0.1, 1]])
-# p2 = np.asarray(pcd.points)
-# print(p2[0])
-
 pcd.voxel_down_sample(voxel_size=0.1)
 o3d.visualization.draw_geometries([pcd])
